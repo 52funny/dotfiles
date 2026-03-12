9f45714b68174f5ee523faebf45f413b2fd1eb82
52funny <self@52funny.me>
2026-03-12 19:18:26 +0800

    feat: add yabai

---
+++ yabai/.config/yabai/focus_display_cycle_next.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 一次性查询所有显示器并计算目标索引
+target_index=$(yabai -m query --displays | jq -r '
+  . as $all | 
+  (map(select(."has-focus" == true)) | .[0].frame.x) as $cx |
+  (map(select(.frame.x > $cx)) | sort_by(.frame.x) | .[0] // ($all | sort_by(.frame.x) | .[0])) | .index
+')
+
+# 仅在获取到有效索引时执行聚焦
+if [ "$target_index" != "null" ] && [ -n "$target_index" ]; then
+  yabai -m display --focus "$target_index"
+fi
