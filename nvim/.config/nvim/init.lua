3ab901987d69ffc9643198c93aee6d555850d3c8
52funny <self@52funny.me>
2026-03-29 18:50:28 +0800

    feat: update nvim theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -984,6 +984,19 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'neanias/everforest-nvim',
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require('everforest').setup {
+        -- Your config here
+      }
+      vim.cmd.colorscheme 'everforest'
+    end,
+  },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
