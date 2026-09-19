5e105121390ed01e52354b9ac17d152b524d8590
52funny <52funny@52funnydeMacBook-Air.local>
2026-09-19 09:12:53 +0800

    fix: update macos pdf reader

--- nvim/.config/nvim/lua/custom/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/custom/plugins/vimtex.lua
@@ -11,7 +11,10 @@ return {
     -- VimTeX configuration goes here, e.g.
     -- For MacOS Skim viewer
     if vim.fn.has 'mac' == 1 then
-      vim.g.vimtex_view_method = 'sioyek'
+      vim.g.vimtex_view_method = 'skim'
+      vim.g.vimtex_view_skim_sync = 1
+      vim.g.vimtex_view_skim_activate = 1
+      vim.g.vimtex_view_skim_reading_bar = 1
     elseif vim.fn.has 'linux' then
       vim.g.vimtex_view_method = 'zathura'
     end
